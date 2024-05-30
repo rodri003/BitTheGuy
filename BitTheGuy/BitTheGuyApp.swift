ab1888567cb53631bf975545c80c61f05ebfd05a
Rafael Rodriguez <rafael.nc@me.com>
2024-05-30 12:26:33 -0400

    Initial Commit

---
+++ BitTheGuy/BitTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitTheGuyApp.swift
+//  BitTheGuy
+//
+//  Created by Rafael Rodriguez on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BitTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

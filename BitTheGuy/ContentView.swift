7065ee05afea26b236eee22f82424636f7410a04
Rafael Rodriguez <rafael.nc@me.com>
2024-05-30 13:29:42 -0400

    Chapter 3.1 BipTheClown

--- BitTheGuy/ContentView.swift
+++ BitTheGuy/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Spacer()
+            Image ("clown")
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                }
+            Spacer()
+            
+            Button(action: {
+                //TODO: Button action here
+            }, label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            })        }
         .padding()
+    }
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print ("😡 ERROR: Could not read file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print ("😡 ERROR: \(error.localizedDescription) playing audioPlayer.")
+            
+        }
     }
 }
 
